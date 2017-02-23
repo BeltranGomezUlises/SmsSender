127820651816825f39abd90cfb26387f01062613
ulises <beltrangomezulises@gmail.com>
2017-02-23 12:28:06 -0700

    SMS permision static

--- app/src/main/java/com/ub/smssender/MainActivity.java
+++ app/src/main/java/com/ub/smssender/MainActivity.java
@@ -7,9 +7,9 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,7 +24,9 @@ import retrofit2.Response;
 
 public class MainActivity extends Activity {
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 1;
+    private static final int PERMISSION_READ_PHONE_STATE = 1;
+    private static final int PERMISSION_SEND_SMS = 2;
+    
     private TextView txtData;
     private Button btnSend;
 
@@ -36,15 +38,25 @@ public class MainActivity extends Activity {
         txtData = (TextView) findViewById(R.id.txtImei);
         btnSend = (Button) findViewById(R.id.btnSend); btnSend.setEnabled(false);
 
-        this.permisos();
+        this.permisos(); //SOLICITAR PERMISOS
 
     }
 
     private void init(){
-        btnSend.setEnabled(true);
-        btnSend.setOnClickListener((view) ->{ System.out.println("algo"); });
-        txtData.setText(this.getDevideImei());
-        this.obtenerMensajes();
+        //si TODOS los permisos esta garantizados
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            btnSend.setEnabled(true);
+            btnSend.setOnClickListener((view) ->{
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage("6671995737", null, "hola dani", null,null);
+            });
+            txtData.setText(this.getDevideImei());
+        }else{
+            System.out.println("ambos permisos no garantizados");
+        }
+
+        //this.obtenerMensajes();
 
         //iniciar servicio de mensajes
 
@@ -56,29 +68,52 @@ public class MainActivity extends Activity {
             ActivityCompat.requestPermissions(
                     MainActivity.this,
                     new String[]{Manifest.permission.READ_PHONE_STATE},
-                    MY_PERMISSIONS_REQUEST_READ_PHONE_STATE
+                    PERMISSION_READ_PHONE_STATE
+            );
+        } else {
+            Log.i("permiso", "read phone state ya concedido");
+            init();
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            //solicitar el permiso
+            ActivityCompat.requestPermissions(
+                    MainActivity.this,
+                    new String[]{Manifest.permission.SEND_SMS},
+                    PERMISSION_SEND_SMS
             );
         } else {
             //permisos ya concedidos
-            this.init();
+            Log.i("permiso", "Send sms ya concedido");
+            init();
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
         switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE: {
+            case PERMISSION_READ_PHONE_STATE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.w("permision", "PERMISO ACCEDIDO");
-                    Toast.makeText(MainActivity.this, "Servicio listo para operar", Toast.LENGTH_LONG).show();;
-                    this.init();
+                    Log.w("permision", "PERMISO ACCEDIDO code: " + PERMISSION_READ_PHONE_STATE);
+                    Toast.makeText(MainActivity.this, "Servicio listo para operar", Toast.LENGTH_LONG).show();
+                    init();
                 } else {
                     //mostrar mensaje de necesida de permiso para funcionar
                     Toast.makeText(MainActivity.this, "Son necesarios permisos para operar", Toast.LENGTH_LONG).show();;
-                    Log.w("permision", "PERMISO DENEGADO");
+                    Log.w("permision", "PERMISO DENEGADO code: " + PERMISSION_READ_PHONE_STATE);
                 }
-                return;
-            }
+                break;
+            case PERMISSION_SEND_SMS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.w("permision", "PERMISO ACCEDIDO code: " + PERMISSION_SEND_SMS);
+                    Toast.makeText(MainActivity.this, "Servicio listo para operar", Toast.LENGTH_LONG).show();;
+                    init();
+                } else {
+                    //mostrar mensaje de necesida de permiso para funcionar
+                    Toast.makeText(MainActivity.this, "Son necesarios permisos para operar", Toast.LENGTH_LONG).show();;
+                    Log.w("permision", "PERMISO DENEGADO code: " + PERMISSION_SEND_SMS);
+                }
+                break;
+            default: Log.i("Atention", "No requestCode"); break;
         }
     }
 
